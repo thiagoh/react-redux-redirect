599f326414f238946a70cee6191e1f5769f98dc1
Thiago Andrade <thiagsou@amazon.com>
2018-01-14 17:54:02 -0500

    Improve console.logs to watch the action -> middlewares -> reducers lifecycle

--- src/middleware.js
+++ src/middleware.js
@@ -1,44 +1,61 @@
 export const createMiddlewares = redirector => {
   const promiseFlattenMiddleware = ({ dispatch }) => {
-    return next => action => {
-      console.log('redirectMiddleware');
-      if (typeof action === 'function') {
-        console.log('resolving promise action');
-        action(dispatch);
-        return;
-      }
-      return next(action);
+    return next => {
+      console.log('install promiseFlattenMiddleware');
+
+      return action => {
+        console.log('redirectMiddleware');
+        if (typeof action === 'function') {
+          console.log('resolving promise action');
+          action(dispatch);
+          return;
+        }
+        return next(action);
+      };
     };
   };
   const redirectMiddleware = ({ dispatch }) => {
-    return next => action => {
-      let fun;
-      if (action.payload && typeof action.payload.redirect === 'function') {
-        fun = () => {
-          action.payload.redirect(redirector);
-        };
-      }
+    return next => {
+      console.log('install redirectMiddleware');
 
-      const value = next(action);
-      console.log('after next');
-      if (fun) fun();
-      return value;
+      return action => {
+        let fun;
+        if (action.payload && typeof action.payload.redirect === 'function') {
+          fun = () => {
+            action.payload.redirect(redirector);
+          };
+        }
+
+        console.log('before next');
+        const value = next(action);
+        console.log('after next');
+        if (fun) fun();
+        return value;
+      };
     };
   };
 
   const firstMiddleware = ({ dispatch }) => {
-    return next => action => {
-      console.log('firstMiddleware');
-      return next(action);
+    return next => {
+      console.log('install firstMiddleware');
+
+      return action => {
+        console.log('firstMiddleware');
+        return next(action);
+      };
     };
   };
 
   const lastMiddleware = ({ dispatch }) => {
-    return next => action => {
-      console.log('lastMiddleware');
-      return next(action);
+    return next => {
+      console.log('install lastMiddleware');
+
+      return action => {
+        console.log('lastMiddleware');
+        return next(action);
+      };
     };
   };
 
-  return [firstMiddleware, redirectMiddleware, lastMiddleware, promiseFlattenMiddleware];
+  return [firstMiddleware, promiseFlattenMiddleware, redirectMiddleware, lastMiddleware];
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { MainReducer } from './main';
 
-const rootReducer = combineReducers({
+const combinedReducer = combineReducers({
   mainReducer: MainReducer
 });
+const rootReducer = (state, action) => {
+  console.log('Hello from rootReducer');
+  return combinedReducer(state, action);
+};
 
 export default rootReducer;
